1a68ea005d69bc5ebec67a3ed763d2bca9307ede
vrouge <dlaghdrbs907@gmail.com>
2024-08-18 07:26:25 +0900

    fix : topic setting

--- src/main/java/com/tetrips/chatv2/handler/ChatWebSocketHandler.java
+++ src/main/java/com/tetrips/chatv2/handler/ChatWebSocketHandler.java
@@ -1,5 +1,6 @@
 package com.tetrips.chatv2.handler;
 
+import com.tetrips.chatv2.Service.KafkaTopicService;
 import com.tetrips.chatv2.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ChatWebSocketHandler {
   private final SimpMessagingTemplate messagingTemplate;
+  private final KafkaTopicService kafkaTopicService;
 
   @KafkaListener(topics = "processed-chat-topic", groupId = "chat-group")
   public void listen(ChatMessage message) {
+
+    kafkaTopicService.createTopicIfNotExists("processed-chat-topic", 180, (short) 3);
     messagingTemplate.convertAndSend("/topic/messages/" + message.getPrId() + "/" + message.getUserId(), message);
   }
 }
